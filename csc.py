179da86b4086dc00e27c6378430ac8ffb04e6bf2
Paul Abt <paul.abt@volkswagen.de>
2022-09-05 02:53:06 +0200

    feat: improve error handling and summary output

--- csc.py
+++ csc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -u
 
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
@@ -11,39 +11,44 @@ import json
 import sys
 
 
-COLOR_DELETE = '\033[31m'
+COLOR_DELETE = '\033[31;1m'
 COLOR_FAIL = '\033[31m'
 COLOR_IRRELEVANT = '\033[34m'
 COLOR_OK = '\033[32m'
 COLOR_RESET = '\033[0m'
 COLOR_WARN = '\033[33m'
 
-COLOR_BULLET = '\033[34m'
-BULLET = ''
+BULLET = f'{COLOR_DELETE}*{COLOR_RESET}'
 
 
 def main():
+
+    args = parse_args()
 
     count_log_groups = 0
     count_log_streams = 0
     count_bytes_before = 0
     count_bytes_after = 0
 
-    args = parse_args()
+    exceptions = []
 
     resp, code = shell_exec(['aws', 'logs', 'describe-log-groups'])
     if code != 0:
-        die(resp, code)
+        log_error(resp, code)
+        exit(code)
+
 
     for group in resp['logGroups']:
 
         count_bytes_before += group['storedBytes']
         group_name = group['logGroupName']
+        stream_name = None
 
         try:
             resp, code = shell_exec(['aws', 'logs', 'describe-log-streams', '--descending', '--log-group-name', group_name]) # sort direction: newest to oldest
             if code != 0:
-                die(resp, code)
+                log_error(resp, code)
+                continue
 
             log_streams = resp['logStreams']
 
@@ -57,7 +62,7 @@ def main():
 
                 if older_than_days(7, latest_log_occurrence):
                     if i == 0: # if the most recent stream contains no entries younger than 7 days, delete the group
-                        print(f'{BULLET}{COLOR_DELETE}deleting group {COLOR_IRRELEVANT}{group_name}{COLOR_RESET}'.strip())
+                        print(f'deleting {COLOR_DELETE}group{COLOR_RESET} {group_name}'.strip())
 
                         if not args.dry:
                             resp, code = shell_exec(['aws', 'logs', 'delete-log-group', '--log-group-name', group_name])
@@ -68,10 +73,10 @@ def main():
                         count_log_groups += 1
                         count_log_streams += len(log_streams)
 
-                        print(f'{COLOR_OK}[OK]{COLOR_RESET} (total: {count_log_groups})')
+                        print(f'{COLOR_OK}[OK] {COLOR_IRRELEVANT}(total: {count_log_groups}){COLOR_RESET}')
                         break
                     else: # if any subsequent stream contains no entries younger than 7 days, delete the stream
-                        print(f"{BULLET}{COLOR_DELETE}deleting stream{COLOR_RESET} {stream_name} from group {group_name}".strip())
+                        print(f"deleting {COLOR_DELETE}stream{COLOR_RESET} {stream_name} from group {group_name}".strip())
 
                         if not args.dry:
                             resp, code = shell_exec(['aws', 'logs', 'delete-log-sream', '--log-group-name', group_name, '--log-stream-name', stream_name])
@@ -83,24 +88,29 @@ def main():
                         print(f'{COLOR_OK}[OK] {COLOR_IRRELEVANT}(total: {count_log_streams}){COLOR_RESET}')
                 else:
                     if i == 0:
-                        print(f'{BULLET}{COLOR_IRRELEVANT}ignoring group {group_name}{COLOR_RESET}')
+                        print(f'{COLOR_IRRELEVANT}ignoring group {group_name}{COLOR_RESET}')
                         continue
-                    print(f"{BULLET}{COLOR_IRRELEVANT}ignoring stream {stream_name} of group {group_name}{COLOR_RESET}")
+                    print(f"{COLOR_IRRELEVANT}ignoring stream {stream_name} of group {group_name}{COLOR_RESET}")
+
+                stream_name = None
 
         except BaseException as e:
-            print(f"{COLOR_WARN}[WARN]{COLOR_RESET} one entry may have been skipped - unexpected problem: {e}")
+            if not stream_name:
+                print(f"{COLOR_WARN}[WARN]{COLOR_RESET} there was a problem with log group {group_name}: {e}")
+                exceptions.append(group_name)
+            else:
+                print(f"{COLOR_WARN}[WARN]{COLOR_RESET} there was a problem with stream {stream_name} of group {group_name}: {e}")
+                exceptions.append(f'{group_name} > {stream_name}')
             continue
 
     resp, code = shell_exec(['aws', 'logs', 'describe-log-groups'])
     if code != 0:
-        die(resp, code)
+        log_error(resp, code)
 
     for group in resp['logGroups']:
         count_bytes_after += group['storedBytes']
 
-    print(f'deleted log groups: {count_log_groups}')
-    print(f'deleted log streams: {count_log_streams}')
-    print(f'total log size before and after execution: {count_bytes_before} -> {count_bytes_after} bytes')
+    print_summary(count_log_groups, count_log_streams, count_bytes_before, count_bytes_after, exceptions)
 
 
 def parse_args():
@@ -126,14 +136,28 @@ def shell_exec(args):
         return result.stderr.decode('utf-8').strip(), result.returncode
         
 
-def die(error, code):
+def log_error(error, code):
     print(f"{COLOR_FAIL}[ERROR]{COLOR_RESET} {error} [exit code: {code}]")
-    exit(code)
 
 
 def older_than_days(time_period, epoch_unix_timestamp):
     converted = datetime.utcfromtimestamp( int(epoch_unix_timestamp / 1000) )
     return converted < (datetime.today() - timedelta(days=time_period))
+
+
+def print_summary(count_log_groups, count_log_streams, count_bytes_before, count_bytes_after, exceptions):
+    print(f'\n{COLOR_RESET}-------------{COLOR_RESET}')
+    print(f'{COLOR_OK}S U M M A R Y{COLOR_RESET}\n')
+
+    print(f'Deleted log groups: {COLOR_OK}{count_log_groups}{COLOR_RESET}')
+    print(f'Deleted log streams: {COLOR_OK}{count_log_streams}{COLOR_RESET}')
+    print(f'Total log size before and after execution: {COLOR_OK}{count_bytes_before}{COLOR_RESET} -> {COLOR_OK}{count_bytes_after}{COLOR_RESET} (bytes)')
+
+    print(f'\nSkipped due to problems (please review and delete manually):')
+    for ex in exceptions:
+        print(BULLET, ex)
+
+    print('')
 
 
 description="""Cleans up your messy crime scene inside CloudWatch :)
